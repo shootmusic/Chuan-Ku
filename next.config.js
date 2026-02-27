e94630bb54a04ff6d3dacbdc81d0bd13c0116a8b
shootmusic <shootmusic@gmail.com>
2026-02-27 21:21:48 +0000

    fix: expose cloudinary env vars in next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['saweria.co'],
-  },
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  }
 }
 
 module.exports = nextConfig
