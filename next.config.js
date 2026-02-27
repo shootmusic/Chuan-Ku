50e1aea43fbca4c93f12ceb1d3c2fe5531657444
shootmusic <shootmusic@gmail.com>
2026-02-27 15:37:38 +0000

    fix: remove invalid experimental.appDir from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['saweria.co'],
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname)
-    return config
-  },
-  experimental: {
-    appDir: true,
   },
 }
 
